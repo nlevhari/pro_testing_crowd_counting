f404d4afb36571823a67ef6a1b72624f0c18cffa
nlevhari <nlevhari@gmail.com>
2023-03-03 17:06:38 +0200

    Downloaded and changed model_path for vgg16_bn Changed torchvision device to CPU Commented some hotfix that created a bug in misc.py

--- run_test.py
+++ run_test.py
@@ -1,4 +1,5 @@
 import argparse
+import glob
 import datetime
 import random
 import time
@@ -42,8 +43,7 @@ def main(args, debug=False):
 
     os.environ["CUDA_VISIBLE_DEVICES"] = '{}'.format(args.gpu_id)
 
-    print(args)
-    device = torch.device('cuda')
+    device = torch.device('cpu')
     # get the P2PNet
     model = build_model(args)
     # move to GPU
@@ -59,42 +59,44 @@ def main(args, debug=False):
         standard_transforms.ToTensor(), 
         standard_transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
+    for im in glob.glob('./protest_images/*'):
+        # set your image path here
+        img_path = im
+        # load the images
+        img_raw = Image.open(img_path).convert('RGB')
+        # round the size
+        width, height = img_raw.size
+        new_width = width // 128 * 128
+        new_height = height // 128 * 128
+        img_raw = img_raw.resize((new_width, new_height), Image.ANTIALIAS)
+        # pre-proccessing
+        img = transform(img_raw)
 
-    # set your image path here
-    img_path = "./vis/demo1.jpg"
-    # load the images
-    img_raw = Image.open(img_path).convert('RGB')
-    # round the size
-    width, height = img_raw.size
-    new_width = width // 128 * 128
-    new_height = height // 128 * 128
-    img_raw = img_raw.resize((new_width, new_height), Image.ANTIALIAS)
-    # pre-proccessing
-    img = transform(img_raw)
+        samples = torch.Tensor(img).unsqueeze(0)
+        samples = samples.to(device)
+        # run inference
+        outputs = model(samples)
+        outputs_scores = torch.nn.functional.softmax(outputs['pred_logits'], -1)[:, :, 1][0]
 
-    samples = torch.Tensor(img).unsqueeze(0)
-    samples = samples.to(device)
-    # run inference
-    outputs = model(samples)
-    outputs_scores = torch.nn.functional.softmax(outputs['pred_logits'], -1)[:, :, 1][0]
+        outputs_points = outputs['pred_points'][0]
 
-    outputs_points = outputs['pred_points'][0]
+        threshold = 0.5
+        # filter the predictions
+        points = outputs_points[outputs_scores > threshold].detach().cpu().numpy().tolist()
+        predict_cnt = int((outputs_scores > threshold).sum())
 
-    threshold = 0.5
-    # filter the predictions
-    points = outputs_points[outputs_scores > threshold].detach().cpu().numpy().tolist()
-    predict_cnt = int((outputs_scores > threshold).sum())
+        outputs_scores = torch.nn.functional.softmax(outputs['pred_logits'], -1)[:, :, 1][0]
 
-    outputs_scores = torch.nn.functional.softmax(outputs['pred_logits'], -1)[:, :, 1][0]
+        outputs_points = outputs['pred_points'][0]
+        # draw the predictions
+        size = 3
+        img_to_draw = cv2.cvtColor(np.array(img_raw), cv2.COLOR_RGB2BGR)
+        for p in points:
+            img_to_draw = cv2.circle(img_to_draw, (int(p[0]), int(p[1])), size, (0, 0, 255), -1)
+        # save the visualized image
+        print(os.path.join(args.output_dir, 'pred{}.jpg'.format(predict_cnt)))
+        cv2.imwrite(os.path.join(args.output_dir, 'pred{}.jpg'.format(predict_cnt)), img_to_draw)
 
-    outputs_points = outputs['pred_points'][0]
-    # draw the predictions
-    size = 2
-    img_to_draw = cv2.cvtColor(np.array(img_raw), cv2.COLOR_RGB2BGR)
-    for p in points:
-        img_to_draw = cv2.circle(img_to_draw, (int(p[0]), int(p[1])), size, (0, 0, 255), -1)
-    # save the visualized image
-    cv2.imwrite(os.path.join(args.output_dir, 'pred{}.jpg'.format(predict_cnt)), img_to_draw)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('P2PNet evaluation script', parents=[get_args_parser()])
